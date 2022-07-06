3d1dd6f4c01e01d957e6e53577856f03cb86a99b
nine <nine@aphlor.org>
2022-07-06 20:43:45 +0100

    some not-quite-working display testing sitting in the tree for a while

--- src/config.h
+++ src/config.h
@@ -18,9 +18,10 @@
 #endif
 
 #if HIDPICO_REVISION == 2
-#  define I2C_PORT i2c0
+#  define I2C_PORT      i2c0
 #  define I2C_PIN_SDA   4
 #  define I2C_PIN_SCL   5
+#  define I2C_IRQN      23
 
 #  define KBD_AMIGA_RST 10
 #  define KBD_AMIGA_DAT 11
@@ -34,9 +35,10 @@
 #  define QM1_AMIGA_B2  26
 #  define QM1_AMIGA_B3  27
 #elif HIDPICO_REVISION == 4
-#  define I2C_PORT i2c1
+#  define I2C_PORT      i2c1
 #  define I2C_PIN_SDA   2
 #  define I2C_PIN_SCL   3
+#  define I2C_IRQN      24 // this is tied to the i2c port being used so get the right one!
 
 #  define KBD_AMIGA_RST 4
 #  define KBD_AMIGA_DAT 5
--- src/main.c
+++ src/main.c
@@ -15,6 +15,8 @@
 #include "platform/amiga/keyboard_serial_io.h"
 #include "platform/amiga/quad_mouse.h"
 #include "util/output.h"
+#include "display/ssd1306.h"
+#include "display/font_6x13.h"
 
 #include "config.h"
 #include "tusb_config.h"
@@ -40,6 +42,23 @@ int main(void)
     gpio_set_function(KBD_AMIGA_CLK, GPIO_FUNC_SIO);
     gpio_set_function(KBD_AMIGA_DAT, GPIO_FUNC_SIO);
 
+    // setup i2c display
+    // @todo this is both not working and using blocking i2c; it needs "something"
+    i2c_init(I2C_PORT, 400000);
+    gpio_set_function(I2C_PIN_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_PIN_SCL, GPIO_FUNC_I2C);
+
+    gpio_pull_up(I2C_PIN_SDA);
+    gpio_pull_up(I2C_PIN_SCL);
+
+    ssd1306_t disp;
+    disp.external_vcc = false;
+    ssd1306_init(&disp, 128, 64, 0x3c, I2C_PORT);
+    ssd1306_clear(&disp);
+
+    ssd1306_draw_string_with_font(&disp, 0, 0, 1, font_6x13, "hid-pico running");
+    ssd1306_show(&disp);
+
     // tinyusb board init; led, uart, button, usb
     board_init();
 
@@ -47,7 +66,7 @@ int main(void)
     ahprintf(welcomeText);
 
     // initialise the usb stack
-    // definition CFG_TUSB_RHPORT0_MODE as OPT_MODE_HOST will put the board into host mode
+    // defining CFG_TUSB_RHPORT0_MODE as OPT_MODE_HOST will put the controller into host mode
     tusb_init();
 
     // we're single arch right now, but in future this should hand off to whatever the
