cb4c53999aed7dcc7a35f5bc335948691cd68fe8
nine <nine@aphlor.org>
2022-07-09 10:53:18 +0100

    debug console tidyup

---
+++ src/util/debug_cons.c
@@ -0,0 +1,92 @@
+/**
+ * this file is part of amigahid-pico, (c) 2021 just nine <nine@aphlor.org>
+ * please locate the full source at https://github.com/borb/amigahid-pico
+ *
+ * released under the terms of the Eclipse Public License 2.0 (EPL-2.0).
+ * please find the complete license text at https://spdx.org/licenses/EPL-2.0
+ *
+ * debug console routines.
+ */
+
+#include "debug_cons.h"
+#include "output.h"
+
+#include <stdint.h>
+
+struct {
+    uint8_t hid_keyboard, hid_mouse, hid_controller;
+    uint8_t plug_events, unplug_events;
+} debug_counters;
+
+void dbgcons_init()
+{
+    ahprintf(
+        VT_ED_CLS "amigahid-pico by nine <nine@aphlor.org>, https://github.com/borb/amigahid-pico"
+    );
+
+    debug_counters.hid_keyboard = 0;
+    debug_counters.hid_mouse = 0;
+    debug_counters.hid_controller = 0;
+    debug_counters.plug_events = 0;
+    debug_counters.unplug_events = 0;
+
+    dbgcons_print_counters();
+}
+
+void dbgcons_print_counters()
+{
+    ahprintf(
+        VT_CUP_POS VT_EL_LIN
+        "[system] key: %02x mouse: %02x joy: %02x total plug: %02x total unplug: %02x",
+        3, 1,
+        debug_counters.hid_keyboard,
+        debug_counters.hid_mouse,
+        debug_counters.hid_controller,
+        debug_counters.plug_events,
+        debug_counters.unplug_events
+    );
+}
+
+void dbgcons_plug(enum debug_plug_types devtype)
+{
+    switch (devtype) {
+        case AP_H_KEYBOARD:
+            debug_counters.hid_keyboard++;
+            break;
+        case AP_H_MOUSE:
+            debug_counters.hid_mouse++;
+            break;
+        case AP_H_CONTROLLER:
+            debug_counters.hid_controller++;
+            break;
+    }
+    debug_counters.plug_events++;
+    dbgcons_print_counters();
+}
+
+void dbgcons_unplug(enum debug_plug_types devtype)
+{
+    switch (devtype) {
+        case AP_H_KEYBOARD:
+            debug_counters.hid_keyboard--;
+            break;
+        case AP_H_MOUSE:
+            debug_counters.hid_mouse--;
+            break;
+        case AP_H_CONTROLLER:
+            debug_counters.hid_controller--;
+            break;
+    }
+    debug_counters.unplug_events++;
+    dbgcons_print_counters();
+}
+
+void dbgcons_amiga_key(uint8_t incode, uint8_t outcode, char updown)
+{
+    ahprintf(
+        VT_CUP_POS VT_EL_LIN
+        "[amigak]  hid in: %02x amiga out: %02x up/down: %c",
+        4, 1,
+        incode, outcode, updown
+    );
+}
---
+++ src/util/debug_cons.h
@@ -0,0 +1,40 @@
+/**
+ * this file is part of amigahid-pico, (c) 2021 just nine <nine@aphlor.org>
+ * please locate the full source at https://github.com/borb/amigahid-pico
+ *
+ * released under the terms of the Eclipse Public License 2.0 (EPL-2.0).
+ * please find the complete license text at https://spdx.org/licenses/EPL-2.0
+ *
+ * debug console routines.
+ */
+
+#ifndef _PLATFORM_COMMON_DEBUG_CONS_H
+#define _PLATFORM_COMMON_DEBUG_CONS_H
+
+#include <stdint.h>
+
+enum debug_plug_types { AP_H_KEYBOARD, AP_H_MOUSE, AP_H_CONTROLLER };
+
+#define ESC         "\033"
+
+#define VT_ED_EOS   ESC "[0J"
+#define VT_ED_BOS   ESC "[1J"
+#define VT_ED_CLS   ESC "[2J"
+#define VT_EL_EOL   ESC "[0K"
+#define VT_EL_BOL   ESC "[1K"
+#define VT_EL_LIN   ESC "[2K"
+
+#define VT_CUP_HOM  ESC "[H"
+#define VT_CUP_POS  ESC "[%d;%dH"
+
+void dbgcons_init();
+
+void dbgcons_print_counters();
+
+void dbgcons_plug(enum debug_plug_types devtype);
+
+void dbgcons_unplug(enum debug_plug_types devtype);
+
+void dbgcons_amiga_key(uint8_t incode, uint8_t outcode, char updown);
+
+#endif // _PLATFORM_COMMON_DEBUG_CONS_H
