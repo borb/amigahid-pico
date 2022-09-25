3d48cc8e3eb6343d5193546247e175305d4dfe72
nine <nine@aphlor.org>
2022-09-25 18:49:30 +0100

    remove older ssd1306 things; prepping for dma code

--- src/main.c
+++ src/main.c
@@ -16,8 +16,6 @@
 #include "platform/amiga/quad_mouse.h"
 #include "util/debug_cons.h"
 #include "util/output.h"
-#include "display/ssd1306.h"
-#include "display/font_6x13.h"
 
 #include "config.h"
 #include "tusb_config.h"
@@ -32,22 +30,7 @@ int main(void)
     gpio_set_function(KBD_AMIGA_CLK, GPIO_FUNC_SIO);
     gpio_set_function(KBD_AMIGA_DAT, GPIO_FUNC_SIO);
 
-    // // setup i2c display
-    // // @todo this is both not working and using blocking i2c; it needs "something"
-    // i2c_init(I2C_PORT, 400000);
-    // gpio_set_function(I2C_PIN_SDA, GPIO_FUNC_I2C);
-    // gpio_set_function(I2C_PIN_SCL, GPIO_FUNC_I2C);
-
-    // gpio_pull_up(I2C_PIN_SDA);
-    // gpio_pull_up(I2C_PIN_SCL);
-
-    // ssd1306_t disp;
-    // disp.external_vcc = false;
-    // ssd1306_init(&disp, 128, 64, 0x3c, I2C_PORT);
-    // ssd1306_clear(&disp);
-
-    // ssd1306_draw_string_with_font(&disp, 0, 0, 1, font_6x13, "hid-pico running");
-    // ssd1306_show(&disp);
+    // i2c things here pls
 
     // tinyusb board init; led, uart, button, usb
     board_init();
